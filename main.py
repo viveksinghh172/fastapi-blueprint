8df013edfb5d121d7b92cb945939c6740851e980
Vivek Singh <viveksinghh172@gmail.com>
2026-04-21 03:06:26 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Message": "Hello World"}
+
+@app.get("/greet")
+def greet():
+    return {"Message": "Hello Vivek"}
