6fac6afc3a5a6713fa14a0e82945d32eaebf103c
Vivek Singh <viveksinghh172@gmail.com>
2026-04-25 04:06:57 +0530

    added query string

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ def read_root():
 @app.get("/greet")
 def greet():
     return {"Message": "Hello Vivek!"}
+
+@app.get("/greet/{name}")
+def greet_name(name:str, age: int):
+    return {"message": f"Hello {name} and you are {age} years old"}
